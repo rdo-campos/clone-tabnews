1a7a4fe290d3081cc041a0d5fc703428547c89e3
Ricardo Campos <64710131+rdo-campos@users.noreply.github.com>
2025-01-08 05:23:51 +0000

    adicionando arquivo .gitignore

---
+++ pages/index.js
@@ -0,0 +1,135 @@
+function Home() {
+    return <h3>
+Senhoras e senhores da turma de 2003 
+<p></p>Filtro solar
+<p></p>Nunca deixem de usar filtro solar
+<p></p>Se eu pudesse dar só uma dica sobre o futuro
+<p></p>Seria esta: usem o filtro solar!
+<p></p>Os benefícios a longo prazo do uso de filtro solar
+<p></p>Estão provados e comprovados pela ciência
+<p></p>Já o resto de meus conselhos não tem outra base
+<p></p>Confiável além de minha própria experiência errante
+<p></p>Mas agora eu vou compartilhar esses conselhos com vocês
+<p></p>Aproveite bem, o máximo que puder: O poder e a beleza da juventude
+<p></p>Ou, então, esquece... Você nunca vai entender mesmo o poder e a beleza da juventude
+<p></p>Até que tenham se apagado
+<p></p>Mas pode crer, daqui a vinte anos
+<p></p>Você vai evocar as suas fotos
+<p></p>E perceber de um jeito que você
+<p></p>Nem desconfia hoje em dia
+<p></p>Quantas, tantas alternativas
+<p></p>Se escancaravam a sua frente
+<p></p>E como você realmente estava com tudo em cima
+<p></p>Você não tá gordo ou gorda
+<p></p>Não se preocupe com o futuro
+<p></p>Ou então preocupe-se, se quiser, mas saiba que
+<p></p>Pré-ocupação é tão eficaz quanto mascar chiclete
+<p></p>Para tentar resolver uma equação de álgebra
+<p></p>As encrencas de verdade de sua vida, tendem a vir de
+<p></p>Coisas que nunca passaram pela sua cabeça preocupada
+<p></p>E te pegam no ponto fraco às 4h da tarde
+<p></p>De uma terça-feira modorrenta
+<p></p>Todo dia, enfrente pelo menos uma coisa
+<p></p>Que te meta medo de verdade
+<p></p>Cante!
+<p></p>Não seja leviano com o coração dos outros
+<p></p>Não ature gente de coração leviano
+<p></p>Use fio dental!
+<p></p>Não perca tempo com inveja
+<p></p>Às vezes se está por cima
+<p></p>Às vezes por baixo
+<p></p>A peleja é longa e, no fim
+<p></p>É só você contra você mesmo
+<p></p>Não esqueça os elogios que receber
+<p></p>Esqueça as ofensas
+<p></p>Se conseguir isso, me ensine
+<p></p>Guarde as antigas cartas de amor
+<p></p>Jogue fora os extratos bancários velhos
+<p></p>Estique-se!
+<p></p>Não se sinta culpado
+<p></p>Por não saber o que fazer da vida
+<p></p>As pessoas mais interessantes que eu conheço
+<p></p>Não sabiam, aos vinte e dois
+<p></p>O que queriam fazer da vida
+<p></p>Alguns dos quarentões mais interessantes
+<p></p>Que eu conheço ainda não sabem
+<p></p>Tome bastante cálcio
+<p></p>Seja cuidadoso com os joelhos
+<p></p>Você vai sentir falta deles
+<p></p>Talvez você case, talvez não
+<p></p>Talvez tenha filhos, talvez não
+<p></p>Talvez se divorcie aos quarenta
+<p></p>Talvez dance ciranda
+<p></p>Em suas bodas de diamante
+<p></p>Faça o que fizer não se auto congratule demais
+<p></p>Nem seja severo demais com você
+<p></p>As suas escolhas tem sempre
+<p></p>Metade das chances de dar certo
+<p></p>É assim para todo mundo
+<p></p>Desfrute de seu corpo
+<p></p>Use-o de toda maneira que puder, mesmo
+<p></p>Não tenha medo de seu corpo
+<p></p>Ou do que as outras pessoas possam achar dele
+<p></p>É o mais incrível instrumento
+<p></p>Que você jamais vai possuir
+<p></p>Dance!
+<p></p>Mesmo que não tenha aonde
+<p></p>Além de seu próprio quarto
+<p></p>Leia as instruções
+<p></p>Mesmo que não vá segui-las depois
+<p></p>Não leia revistas de beleza
+<p></p>Elas só vão fazer você se achar feio!
+
+<p></p>Dedique-se a conhecer os seus pais
+<p></p>É impossível prever
+<p></p>Quando eles terão ido embora, de vez
+<p></p>Seja legal com seus irmãos
+<p></p>Eles são a melhor ponte
+<p></p>Com o seu passado
+<p></p>E possivelmente
+<p></p>Quem vai sempre mesmo
+<p></p>Te apoiar no futuro
+<p></p>Entenda que amigos vão e vem
+<p></p>Mas nunca abra mão de uns poucos e bons
+<p></p>Esforce-se de verdade para diminuir
+<p></p>As distâncias geográficas e de estilos de vida
+<p></p>Porque quanto mais velho você ficar
+<p></p>Mais você vai precisar das pessoas
+<p></p>Que você conheceu quando jovem
+<p></p>More uma vez em Nova York
+<p></p>Mas vá embora antes de endurecer
+<p></p>More uma vez no Havaí
+<p></p>Mas se mande antes de amolecer
+<p></p>Viaje!
+<p></p>Aceite certas verdades inescapáveis
+<p></p>Os preços vão subir, os políticos vão saracotear
+<p></p>Você também vai envelhecer!
+<p></p>E quando isso acontecer
+<p></p>Você vai fantasiar que quando era jovem
+<p></p>Os preços eram razoáveis
+<p></p>Os políticos eram decentes
+<p></p>E as crianças respeitavam os mais velhos
+<p></p>Respeite os mais velhos
+<p></p>E não espere que ninguém segure a sua barra
+<p></p>Talvez você arrume uma boa aposentadoria privada
+<p></p>Talvez case com um bom partido
+<p></p>Mas não esqueça que um dos dois
+<p></p>Pode de repente acabar
+<p></p>Não mexa demais nos cabelos
+<p></p>Se não quando você chegar
+<p></p>Aos 40 vai aparentar 85
+<p></p>Cuidado com os conselhos que comprar
+<p></p>Mas seja paciente com aqueles que os oferecem
+<p></p>Conselho é uma forma de nostalgia
+<p></p>Compartilhar conselhos é um jeito
+<p></p>De pescar o passado do lixo, esfregá-lo
+<p></p>Repintar as partes feias
+<p></p>E reciclar tudo por mais do que vale
+<p></p>Mas no filtro solar acredite!
+
+
+
+    </h3>
+}
+
+export default Home;
